e9623998c713ef28e6d51a3ab0d4020839b9693e
Tomas Votava <tomas.votava@bizztreat.com>
2018-08-04 13:39:43 +0200

    Initial commit

---
+++ www/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return = [
+	#Global settings
+	'global' => [
+		'setup' => FALSE,					#TRUE/FALSE, whether installation has been setup
+		'serverName' => 'localhost',		#Server Name, e.g. https://example.com
+	],
+	#MySQL connection
+	'db' => [
+		'host' => 'localhost',
+		'db' => 'database_name',
+		'user' => 'user',
+		'pass' => 'secret_password'
+	]
+];
+
+?>
